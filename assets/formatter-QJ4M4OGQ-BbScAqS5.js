dec2654067ba1166fc7e4f6e990448782d847769
pierrebaronqumu <pierrebaronqumu@users.noreply.github.com>
2025-12-12 16:21:51 +0000

    deploy: 4bd384bfc54023cac3cb5273c38c7e6ddd7843da

---
+++ assets/formatter-QJ4M4OGQ-BbScAqS5.js
@@ -0,0 +1,1 @@
+import{d as i,b as m}from"./DocsRenderer-GHJI37HO-hstB5RFy.js";import{K as o}from"./iframe-BMxiNplM.js";import"./preload-helper-PPVm8Dsz.js";var t=o(m(),1),d=(0,t.default)(2)(async(e,r)=>e===!1?r:i(r));export{d as formatter};
