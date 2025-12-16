25416447d3a2e067fbceb2ae373cff7f7529baee
pierrebaronqumu <pierre.baron@enghouse.com>
2025-12-16 14:52:12 +0000

    WIP

--- packages/lib/src/components/not-found.tsx
+++ packages/lib/src/components/not-found.tsx
@@ -1,11 +1,11 @@
 import ImageBrokenIcon from '@/icons/image-broken.svg?react';
+import { useI18n } from '@/i18n';
 
-// TODO translate me!
 export function NotFoundComponent() {
   return (
     <div class="qc-not-found">
-      <ImageBrokenIcon width={48} height={48} />
-      <div>Presentation not found</div>
+      <ImageBrokenIcon width={48} height={48}/>
+      <div>{ useI18n().t('common.Presentation not found') }</div>
     </div>
   );
 }
