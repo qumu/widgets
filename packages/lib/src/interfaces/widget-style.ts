51e64018662610383416ec4233611bc13c7dc1a6
pierrebaronqumu <pierre.baron@enghouse.com>
2026-01-26 10:23:09 +0000

    QENG-5823: Moved the CSS Variables in the configuration

---
+++ packages/lib/src/interfaces/widget-style.ts
@@ -0,0 +1,49 @@
+export type Position = 'top-left' | 'top' | 'top-right' | 'left' | 'center' | 'right' | 'bottom-left' | 'bottom' | 'bottom-right';
+
+export interface WidgetStyle {
+  borderRadius: string;
+  closeButton: Partial<{
+    activeBackgroundColor: string;
+    activeColor: string;
+    backgroundColor: string;
+    boxShadow: string;
+    color: string;
+    hoverBackgroundColor: string;
+    hoverColor: string;
+    iconSize: string;
+    padding: string;
+  }>;
+  dialog: Partial<{
+    backdropColor: string;
+    backgroundColor: string;
+    border: string;
+    borderRadius: string;
+    maxWidth: string;
+    padding: string;
+    width: string;
+  }>;
+  height: string;
+  notFound: Partial<{
+    backgroundColor: string;
+    border: string;
+    color: string;
+    iconColor: string;
+  }>;
+  playButton: Partial<{
+    activeBackgroundColor: string;
+    activeColor: string;
+    backgroundColor: string;
+    color: string;
+    height: string;
+    hoverBackgroundColor: string;
+    hoverColor: string;
+    margin: string;
+    padding: string;
+    position: Position;
+    width: string;
+  }>;
+  thumbnail: Partial<{
+    imageFit: string;
+  }>;
+  width: string;
+}
